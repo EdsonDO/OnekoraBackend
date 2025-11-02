35c634d4ae9392c92c233458639bc6174644ce14
EdsonDO <2021110367@udh.edu.pe>
2025-11-02 15:54:00 -0500

    Primer Commit: Avance n°1
    Este commit establece la configuración central del proyecto Django (onekoraAPI/settings.py) y la estructura de la app.
    
    - Crea y registra la app principal api donde residirá la lógica de negocio (models.py, views.py).
    - Conecta Django a la base de datos MySQL (onekora_db) y configura python-dotenv para cargar la contraseña de forma segura desde el archivo .env.
    - Integra djangorestframework y django-cors-headers, configurando CORS_ALLOW_ALL_ORIGINS = True para permitir conexiones de los frontends en desarrollo. (Implementación futura de momento, ya veremos que pasa)
    - Implementa el traductor (djangorestframework-camel-case) para que la API maneje snake_case internamente y sirva camelCase a los clientes.
    - Registra fcm_django para la futura implementación de notificaciones push.

---
+++ onekoraAPI/onekoraAPI/settings.py
@@ -0,0 +1,154 @@
+
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-^h^ob6vzec052qs4va!vd4m%qa&l=3w)-@-etxsg14nql)s*+d'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',        
+    'corsheaders',           
+    'fcm_django',          
+    'api',
+]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'onekoraAPI.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'onekoraAPI.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  # Cambiamos el motor
+        'NAME': 'onekora_db',                   # El nombre de nuestra BD
+        'USER': 'root',                         # Tu usuario de MySQL (usualmente 'root')
+        'PASSWORD': os.getenv('DB_PASSWORD'),   # ¡Lee el secreto del .env!
+        'HOST': 'localhost',                    # O '127.0.0.1'
+        'PORT': '3306',                         # Puerto por defecto de MySQL
+        'OPTIONS': {
+            'sql_mode': 'STRICT_TRANS_TABLES',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# CONFIGURACIÓN DEL "GUARDIA" CORS
+
+# Para la demo, permitiremos cualquier origen (temporalmente)
+
+CORS_ALLOW_ALL_ORIGINS = True 
+
+# Nota de Edson: Voy a dejar esto acá para la
+# futura implementación de lo que quiero hacer (incluida la pagina web)
+# Los nombres de onekoramobile.com y onekoraweb.com son provisionales no se lo tomen enserio
+# No descomentar esto hasta que se haya implementado la web y la app movil, porfis
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000", # Esto es para cuando se conecte a Angular)
+#     "http://localhost:8081", # React Native (si corre ahí)
+#     "http://onekoramobile.com",
+#     "http://onekoraweb.com",
+# ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ),
+}
